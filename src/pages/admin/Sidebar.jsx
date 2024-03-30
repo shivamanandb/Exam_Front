9e0364c89c741e4147b4b49a7cfdb2e8851cb0ba
Shivam Anand Bhushan <shivamanandb@gmail.com>
2024-03-31 00:07:04 +0530

    logout issue fixed

--- src/pages/admin/Sidebar.jsx
+++ src/pages/admin/Sidebar.jsx
@@ -24,108 +24,107 @@ export const Sidebar = () => {
   const logoutHandler = () => {
     dispatch(logout(navigate))
   }
-  
+
   return (
     <div>
-        <Paper variant="elevation" elevation={2} className='p-1' sx={{ width: '100%', maxWidth: 967, bgcolor: 'background.paper' }}>
-          
-          <div className='w-full p-1 mb-8 border border-dashed rounded-md border-rose-800 border-y-2'>
+      <Paper variant="elevation" elevation={2} className='p-1' sx={{ width: '100%', maxWidth: 967, bgcolor: 'background.paper' }}>
+
+        <div className='w-full p-1 mb-8 border border-dashed rounded-md border-rose-800 border-y-2'>
           <ListItemText>
             <div className='flex items-center justify-between gap-7'>
               <div className='flex items-center justify-center mx-auto text-lg'>
                 Menu
               </div>
             </div>
-            </ListItemText>
-          </div>
+          </ListItemText>
+        </div>
         <NavLink to="/admin/home">
-            <ListItem disablePadding>
+          <ListItem disablePadding>
             <ListItemButton className='flex items-start justify-start gap-7'>
-            <div className='flex items-start justify-normal'>
-            <ListItemAvatar>
-                <IoMdHome size={25} />
-            </ListItemAvatar>
-            <ListItemText><div className='text-lg '>Home</div></ListItemText>
-            </div>
+              <div className='flex items-start justify-normal'>
+                <ListItemAvatar>
+                  <IoMdHome size={25} />
+                </ListItemAvatar>
+                <ListItemText><div className='text-lg '>Home</div></ListItemText>
+              </div>
             </ListItemButton>
-            </ListItem>
+          </ListItem>
         </NavLink>
         <NavLink to="/admin/myProfile">
-        <ListItem disablePadding>
-        <ListItemButton className='flex items-start justify-start'>
-          <ListItemAvatar>
-              <FaUserCircle size={25}/>
-          </ListItemAvatar>
-          <ListItemText><div className='text-lg '>Profile</div></ListItemText>
-          </ListItemButton>
-        </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton className='flex items-start justify-start'>
+              <ListItemAvatar>
+                <FaUserCircle size={25} />
+              </ListItemAvatar>
+              <ListItemText><div className='text-lg '>Profile</div></ListItemText>
+            </ListItemButton>
+          </ListItem>
         </NavLink>
 
         <NavLink to="/admin/categories">
-            <ListItem disablePadding>
+          <ListItem disablePadding>
             <ListItemButton className='flex items-start justify-start gap-7'>
-            <div className='flex items-start justify-normal'>
-            <ListItemAvatar>
-                <BiSolidCategory size={25} />
-            </ListItemAvatar>
-            <ListItemText><div className='text-lg '>Categories</div></ListItemText>
-            </div>
+              <div className='flex items-start justify-normal'>
+                <ListItemAvatar>
+                  <BiSolidCategory size={25} />
+                </ListItemAvatar>
+                <ListItemText><div className='text-lg '>Categories</div></ListItemText>
+              </div>
             </ListItemButton>
-            </ListItem>
+          </ListItem>
         </NavLink>
 
         <NavLink to="/admin/quizzes">
-            <ListItem disablePadding>
+          <ListItem disablePadding>
             <ListItemButton className='flex items-start justify-start gap-7'>
-            <div className='flex items-start justify-normal'>
-            <ListItemAvatar>
-                <MdQuiz size={25} />
-            </ListItemAvatar>
-            <ListItemText><div className='text-lg '>Quizzes</div></ListItemText>
-            </div>
+              <div className='flex items-start justify-normal'>
+                <ListItemAvatar>
+                  <MdQuiz size={25} />
+                </ListItemAvatar>
+                <ListItemText><div className='text-lg '>Quizzes</div></ListItemText>
+              </div>
             </ListItemButton>
-            </ListItem>
+          </ListItem>
         </NavLink>
 
         <NavLink to="/admin/addCategory">
-            <ListItem disablePadding>
+          <ListItem disablePadding>
             <ListItemButton className='flex items-start justify-start gap-7'>
-            <div className='flex items-start justify-normal'>
-            <ListItemAvatar>
-                <BiSolidMessageSquareAdd size={25} />
-            </ListItemAvatar>
-            <ListItemText><div className='text-lg '>Add Categories</div></ListItemText>
-            </div>
+              <div className='flex items-start justify-normal'>
+                <ListItemAvatar>
+                  <BiSolidMessageSquareAdd size={25} />
+                </ListItemAvatar>
+                <ListItemText><div className='text-lg '>Add Categories</div></ListItemText>
+              </div>
             </ListItemButton>
-            </ListItem>
+          </ListItem>
         </NavLink>
 
         <NavLink to="/admin/addQuiz">
-            <ListItem disablePadding>
+          <ListItem disablePadding>
             <ListItemButton className='flex items-start justify-start gap-7'>
-            <div className='flex items-start justify-normal'>
-            <ListItemAvatar>
-                <IoMdAdd size={25} />
-            </ListItemAvatar>
-            <ListItemText><div className='text-lg '>Add Quiz</div></ListItemText>
-            </div>
+              <div className='flex items-start justify-normal'>
+                <ListItemAvatar>
+                  <IoMdAdd size={25} />
+                </ListItemAvatar>
+                <ListItemText><div className='text-lg '>Add Quiz</div></ListItemText>
+              </div>
             </ListItemButton>
-            </ListItem>
+          </ListItem>
         </NavLink>
 
-        <NavLink to="/admin/quizzes">
-            <ListItem disablePadding>
-            <ListItemButton className='flex items-start justify-start gap-7'>
+
+        <ListItem disablePadding>
+          <ListItemButton className='flex items-start justify-start gap-7'>
             <div className='flex items-start justify-normal'>
-            <ListItemAvatar>
+              <ListItemAvatar>
                 <IoLogOutSharp size={25} />
-            </ListItemAvatar>
-            <ListItemText><div className='text-lg' onClick={logoutHandler}>Logout</div></ListItemText>
+              </ListItemAvatar>
+              <ListItemText><div className='text-lg' onClick={logoutHandler}>Logout</div></ListItemText>
             </div>
-            </ListItemButton>
-            </ListItem>
-        </NavLink>
-        
+          </ListItemButton>
+        </ListItem>
+
       </Paper>
     </div>
   )
